81f5f9248e20f4b4f273970a91afef4b9b5fb902
Daisuke-23 <md-b8c6.p16w@i.softbank.jp>
2020-09-17 19:37:23 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root to: "items#index"
 
-  resources :users, only: [:index, :edit, :update, :create]
+  # resources :users, only: [:index, :edit, :update, :create]
 
   resources :items do
   resources :orders,  only: [:index, :create]
