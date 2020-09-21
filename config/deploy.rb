8ede8caa482846822ee5dfd03b12b5fda31a3f99
Daisuke-23 <md-b8c6.p16w@i.softbank.jp>
2020-09-21 16:35:07 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.1'
 set :application, 'furima_29205'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:daisuke-23/furima-29205
+set :repo_url,  'git@github.com:daisuke-23/furima-29205'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
